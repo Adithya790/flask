0af60a0bcf96d3b9a4923b6ec27ec34fc0263569
mishal200 <muhammedmishalc.kmechil@gmail.com>
2026-05-11 19:44:06 +0530

    style

--- app.py
+++ app.py
@@ -1,28 +1,73 @@
-from flask import Flask,render_template,request,redirect
-
+from flask import Flask, render_template, request,redirect
+import sqlite3
 app = Flask(__name__)
-users_list = [
-    {'id':1,'name':'adithya','email':'adithya@gmail.com'},
-    {'id':2,'name':'adi','email':'adi@gmail.com'},
-    {'id':3,'name':'appu','email':'appu@gmail.com'},
-]
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_table():
+    conn = get_db_connection()
+    conn.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL
+            )
+    """)
+    conn.commit()
+    conn.close()
+
+create_table()
+
 @app.route('/')
 def home():
+    #logic
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'About page'
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page'
 
 @app.route('/users')
 def users():
-    return render_template('users.html','name'='adithya',users=users_list)
+    conn = get_db_connection()
+    user_list = conn.execute(
+        'SELECT * FROM users'
+    ).fetchall()
+    conn.close()
+    return render_template('users.html',name="adhi", users =user_list)
+
 @app.route('/submit',methods = ['POST'])
 def add_user():
     name = request.form['name']
     email = request.form['email']
-    id = len(users_list)+1
-    users_list.append({'id':id,'name':name,'email':email})
-    return redirect('users')
+    
+    conn = get_db_connection()
+    
+    conn.execute('''
+            INSERT INTO users (name,email) VALUES (?,?)  
+    ''',(name,email))
+    
+    conn.commit()
+    conn.close()
+    
+    return redirect('/users')
+
 @app.route('/users/<int:id>')
 def user(id):
-    return f"id is{id}"
+    print(id)
+    conn = get_db_connection()
+    user_details = conn.execute(
+        'SELECT * FROM users WHERE id = ?',(id,)
+    ).fetchone()
+    return render_template('user.html',user = user_details)
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
